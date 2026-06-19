bafc2566e8b9e68aed15460c98fe2c3e1c094ed6
Sdg877 <sdrakegill@gmail.com>
2026-06-19 07:53:32 +0100

    Update to about page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      
+      <p>
+        The Fast Minds Club was created by Sylvia Drake-Gill, a sole developer
+        from London, England. Sylvia was diagnosed with severe ADHD-C in 2025.
+        After her diagnosis, she struggled to find an app that had all the
+        features she wanted and needed so she decided to build her own from
+        scratch. Using her own experiences and a focus group of people with
+        ADHD, Sylvia built a planner that not only would allow users to schedule
+        tasks and set reminders in Google Calendar, it also tracked a users
+        energy levels ensuring they do not burnout.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
